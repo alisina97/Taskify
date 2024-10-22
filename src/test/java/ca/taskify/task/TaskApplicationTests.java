5e8b9af8917eae7f59e618ffe2e28c9eb97092a3
Ali Sina <ali.sina1447@gmail.com>
2024-10-22 18:18:20 -0400

    initial commit

---
+++ src/test/java/ca/taskify/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.taskify.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
